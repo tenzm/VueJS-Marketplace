11e52a57beed7b5c324fcac13fd2e7c62957abad
Блинов Максим <tenzm@mail.ru>
2019-05-18 14:28:29 +0300

    added axios-api

--- src/store/modules/news.js
+++ src/store/modules/news.js
@@ -1,24 +1,28 @@
-export default({
-    state: {
-      news:[
-        {
-          title: "Хранилище",
-          text: "Заработало!",
-          img: 'coffee.jpg',
-        }
-      ]
-    },
-    mutations: {
-      add_news(state, article){
-        state.news.push(article);
-      },
-      set_news(state, news){
-        state.news = news;
+import api from '@/api';
+export default {
+  state: {
+    news: [
+      {
+        title: "Хранилище",
+        text: "Заработало!",
+        img: "coffee.jpg"
       }
+    ]
+  },
+  mutations: {
+    add_news(state, article) {
+      state.news.push(article);
     },
-    actions: {
-      add_article({state, commit}, article){
-        commit("add_news", article);
-      }
+    set_news(state, news) {
+      state.news = news;
     }
-  });
+  },
+  actions: {
+    add_article({ state, commit }, article) {
+      api.axios.put(api.urls["news"], article).then(res=>{
+        console.log(res.data);
+      });
+      //commit("add_news", article);
+    }
+  }
+};
