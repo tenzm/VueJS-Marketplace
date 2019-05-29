2836874659a091a50f1e3c4229fda84188e3cc0f
Blinov Maksim <tenzm@mail.ru>
2019-05-29 16:46:07 +0300

    added registration interface

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -1,8 +1,32 @@
 <template>
-  <div>
-    <input v-model="username" type="text" placeholder="username">
-    <input v-model="password" type="text" placeholder="password">
-    <button @click="register">Зарегистрироваться</button>
+<div class = "coverdiv">
+  <v-parallax dark :src="require('@/assets/news_header.jpg')" height="150">
+      <v-layout align-center column justify-center>
+        <h1 class="display-2 font-weight-thin mb-3">Регистрация</h1>
+      </v-layout>
+    </v-parallax>
+  <div class = "logdiv">
+    <span style="font-size: 18px; font-style: italic; padding-left: 10px;" >Имя пользователя:</span>
+    <v-flex xs12 style="margin-top: 10px;">
+      <v-text-field
+        v-model="username"
+        type="text"
+        label="Укажите имя пользователя"
+        solo
+      ></v-text-field>
+    </v-flex>
+    <span style="font-size: 18px; font-style: italic; padding-left: 10px; " >Пароль:</span>
+    <v-flex xs12 style="margin-top: 10px;">
+      <v-text-field
+        v-model="password"
+        type="password"
+        label="Укажите пароль"
+        solo
+      ></v-text-field>
+    </v-flex>
+    <v-btn round color="blue darken-4" style="width:150px;" @click="register" dark>Регистрация</v-btn>
+    <router-link to = "/login" style = "font-size: 16px; margin-left: 20px;">Уже есть аккаунт?</router-link>
+  </div>
   </div>
 </template>
 
