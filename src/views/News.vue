3a1827816d2c1dcbba3b7bd7c479d57f3d6e42d6
Blinov Maksim <tenzm@mail.ru>
2019-06-07 22:11:02 +0300

    added adaptive news

--- src/views/News.vue
+++ src/views/News.vue
@@ -10,14 +10,19 @@
           <v-btn round color="blue darken-2" style="color: white; margin-top:10px;" large v-if="auth" @click="go_to_news">Добавить</v-btn>
         </div>
       </v-flex>
-    <div style="margin: 0px auto; width:800px; margin-top: 20px; padding-bottom: 60px">
+
+    <v-container grid-list-xs>
+      <v-layout row wrap>
+        <v-flex xs10 offset-xs1 class="clearfix" style="text-align: center; margin-top: 50px;">
+            <div style="width: 100%; display: inline-block; vertical-align: top; text-align:left;">
+    <div style="width:100%; margin-top: 20px; padding-bottom: 60px">
       <div v-for="(item, key) in news" :key="key" style="margin-top: 20px;" class = "news">
     <span v-if="key == 0 || is_not_equal_date(news[key].createdAt, news[key-1].createdAt)" class="headline font-weight-light" style="margin-top:30px;">{{formatDate(item.createdAt)}}</span>
       <v-hover style="margin-top: 10px;">
         <v-card slot-scope="{ hover }" :class="`elevation-${hover ? 12 : 2}`" class="mx-auto">
-          <v-card-title>
-            <v-img :src="item.img" aspect-ratio="1" height="150"></v-img>
-            <div style="margin-left: -20px; width: 400px;">
+          <v-card-title style="">
+            <v-img :src="item.img" aspect-ratio="1" height="150" width="150" class = "news-img"></v-img>
+            <div style="width: 500px; float: left;" class="news-text">
               <span class="headline">{{item.title}}</span>
               <br>
               <span class="subheading">{{item.text}}</span>
@@ -28,6 +33,10 @@
       </v-hover>
       </div>
     </div>
+            </div>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
@@ -74,8 +83,37 @@ export default {
 };
 </script>
 <style>
-.news .v-image__image--cover {
+
+
+.news .v-image__image.v-image__image--cover {
+    width: 150px;
+}
+
+@media screen and (max-width: 900px) {
+    .news .v-image__image--cover {
     width: 150px !important;
     background-size: cover;
 }
+.news .v-card__title {
+    display: inline-block;
+    text-align: center;
+    width: 100%;
+}
+
+.news .v-responsive.v-image {
+    display: inline-block;
+    text-align: center;
+    margin-top: 15px;
+}
+
+.news .news-text{
+    width: 100% !important;
+    margin: 15px 0;
+}
+
+.news .news-img{
+  width: 150px;
+}
+  }
+
 </style>
